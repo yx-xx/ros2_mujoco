8e3b2622fe6e5aab302987c0d8a12a61c28ede9e
yx-xx <2023959620@qq.com>
2025-08-30 10:20:35 +0800

    添加mujocoRos2Simulator

--- ros2_mujoco_robot/setup.py
+++ ros2_mujoco_robot/setup.py
@@ -20,7 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'robot_controller = ros2_mujoco_robot.robot_controller:main',
+            'mujocoRos2Simulator = ros2_mujoco_robot.mujocoRos2Simulator:main',
             'test = ros2_mujoco_robot.test:main',
         ],
     },
